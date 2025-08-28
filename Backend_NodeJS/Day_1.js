c28b98d2ea8901d6265c2af8d112298f96b9e4e9
Rohanreddy-dev-106 <rohan.reddy.dev106@gmail.com>
2025-08-28 22:48:14 +0530

    Server

---
+++ Backend_NodeJS/Day_1.js
@@ -0,0 +1,40 @@
+//TODO:27-08-25
+//Blocking and unblocking codes
+
+//Creating an HTTP server
+
+const http = require('http');//Object
+const fs = require('fs');//Object
+// console.log(typeof http);
+
+const server = http.createServer((req, res) => {
+
+  // res.write("This is My Application  ")//Modfying response befor end
+  const data = fs.readFileSync("index.html").toString();//render HTML
+  if (req.url.includes("/products")) {
+    res.end("product Page...");
+    // console.log(req.url);
+    return; // stop execution after sending response
+  }
+  else if (req.url.includes("/users")) {
+    res.end("users page..");
+    return;
+  }
+  res.end(data);//Sends HTML String to Client
+  return;
+});
+server.listen(3100, () => {
+  console.log("Server is started....");
+});
+
+
+
+
+
+
+
+
+
+
+
+
