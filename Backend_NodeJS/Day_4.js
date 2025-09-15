833358eac0b972f39d2b35720bf390431e389d3c
Rohanreddy-dev-106 <rohan.reddy.dev106@gmail.com>
2025-09-15 16:10:51 +0530

    Notes

--- Backend_NodeJS/Day_4.js
+++ Backend_NodeJS/Day_4.js
@@ -1,4 +1,16 @@
 //TODO: 13-09-25
+/*express (function)
+│
+├─ .static()    // method attached to function
+├─ .json()
+├─ .urlencoded()
+│
+└─ () → returns app object
+        │
+        ├─ .get()
+        ├─ .post()
+        ├─ .use()
+        └─ etc.*/
 
 //Start With Express Js
 
@@ -11,7 +23,7 @@
 const express = require('express');//Express is a factory function. it is a special type of function which can acts as object by keys in it.
 const fs=require('fs');
 
-const server=express();
+const server=express();// it returns a server object(type function)
 
 function firstMiddleware(req,res,next){
 if (req.url==="/") {
@@ -20,23 +32,28 @@ if (req.url==="/") {
 }
 }
 function SecondMiddleware(req,res,next){
-    console.log("secode middleware..");
-   const data= fs.readFileSync("index.html",{encoding:"utf-8"});
-    res.send(data);
-    // next()
+//     console.log("secode middleware..");
+//    const data= fs.readFileSync("index.html",{encoding:"utf-8"});
+    console.log("seconde middleware");
+    
+    next()
 }
 function GlobelMideleware(req,res,next){
     console.log("Application-Level-Midellware");
     next();
 
 }
-server.use(GlobelMideleware);
+server.use(GlobelMideleware);//app.use()  to excute globel middlevares
+
 const excute=[firstMiddleware,SecondMiddleware];
 
 server.get("/",excute,(req,res)=>{
-    // res.send("This is From Res 2")
+    // res.status(200);
+    res.status(200).set("Content-type","text/plane");//In Express, res.status() sets the HTTP status code and returns the same res (response) object, so you can chain more methods on it.
+    res.send("This is From Res 2")
     return;
 })
+server.use(express.static("public"));//Static HTML files
 const PORT =8080;
 
 server.listen(PORT,()=>console.log("Server is started...")
